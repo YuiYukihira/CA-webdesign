f94bd2aa9b233e0404f277daa7bd58926cbec9cd
Yui Yukihira <yuiyukihira@pm.me>
2019-03-28 11:02:14 +0000

    some cleanup to match uni guidelines

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-# See https://hub.docker.com/_/php/
-FROM php:7.3.3-apache
-
-# Copy source code into /var/www/html inside the container
-COPY ./db_conn.php /var/www/
-COPY ./src /var/www/html/
--- db_conn.php
+++ db_conn.php
@@ -1,7 +1,7 @@
 <?php
 // Database cannections should be moved into a file outside of the publicly accessable directory.
-$dbservername = "localhost";
-$dbusername = "ECM1417";
-$dbpassword = "ECM1417";
-$dbdatabase = "Coursework";
+$dbservername = "emps-sql.ek.ac.uk";
+$dbusername = "as1200";
+$dbpassword = "as1200";
+$dbdatabase = "as1200";
 ?>
